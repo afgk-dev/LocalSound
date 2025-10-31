8b0291128f042cdef6c4abf9573646d27e29e22e
kaleb <cardtunic@gmail.com>
2025-10-31 20:24:55 -0300

    refactor(AudioFileRepository): filter music base on IS_MUSIC column + get uri from ContentUris.withAppendedId

--- app/src/main/java/dev/afgk/localsound/data/audioFiles/AudioFileRepository.kt
+++ app/src/main/java/dev/afgk/localsound/data/audioFiles/AudioFileRepository.kt
@@ -1,10 +1,11 @@
 package dev.afgk.localsound.data.audioFiles
 
+import android.content.ContentUris
 import android.content.Context
+import android.os.Build
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
-import java.io.File
 
 class AudioFilesRepository(private val context: Context) {
     fun loadFiles(): List<AudioFile> {
@@ -19,24 +20,27 @@ class AudioFilesRepository(private val context: Context) {
             val title = MediaStore.Audio.Media.TITLE
             val artist = MediaStore.Audio.Media.ARTIST
             val duration = MediaStore.Audio.Media.DURATION
-
-            /** TODO: Use ContentResolver instead of this. Doesn't work in Android 10+ due to Scoped Storage */
-            val path = MediaStore.Audio.Media.DATA
+            val isMusic = MediaStore.Audio.Media.IS_MUSIC
         }
 
         val projection = arrayOf(
             cols.id,
             cols.title,
             cols.artist,
-            cols.duration,
-            cols.path
+            cols.duration
         )
-        val selection = "${cols.path} LIKE ?"
-        val selectionArgs = arrayOf("${musicFolder}${File.separator}%")
+        val selection = "${cols.isMusic} = ?"
+        val selectionArgs = arrayOf("1")
         val sortOrder = "${cols.title} ASC"
 
+        val collection =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) MediaStore.Audio.Media.getContentUri(
+                MediaStore.VOLUME_EXTERNAL
+            )
+            else MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+
         val cursor = context.contentResolver.query(
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+            collection,
             projection,
             selection,
             selectionArgs,
@@ -49,13 +53,13 @@ class AudioFilesRepository(private val context: Context) {
                 val titleIdx = cursor.getColumnIndexOrThrow(cols.title)
                 val artistIdx = cursor.getColumnIndexOrThrow(cols.artist)
                 val durationIdx = cursor.getColumnIndexOrThrow(cols.duration)
-                val pathIdx = cursor.getColumnIndexOrThrow(cols.path)
 
                 val id = cursor.getLong(idIdx)
                 val title = cursor.getString(titleIdx)
                 val artist = cursor.getString(artistIdx)
                 val duration = cursor.getInt(durationIdx)
-                val path = cursor.getString(pathIdx)
+
+                val path = ContentUris.withAppendedId(collection, id).toString()
 
                 audioFiles.add(AudioFile(id, title, artist, duration, path))
             }
