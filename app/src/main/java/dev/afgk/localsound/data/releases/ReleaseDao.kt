84be3d2dc10971d2472026660adc44206093fa92
Felipe <felipe10ferreira2016@gmail.com>
2025-11-17 17:44:16 -0300

    feat(data): Implement media synchronization with ContentObserver, by comparing the differences in the URI on the db and storage

--- app/src/main/java/dev/afgk/localsound/data/releases/ReleaseDao.kt
+++ app/src/main/java/dev/afgk/localsound/data/releases/ReleaseDao.kt
@@ -4,17 +4,10 @@ import androidx.room.Dao
 import androidx.room.Query
 import dev.afgk.localsound.data.core.BaseDao
 
-
-
-class ReleaseData(
-    val name: String,
-    val coverArtUri: String
-)
-
 @Dao
 interface ReleaseDao: BaseDao<ReleaseEntity> {
 
     //Get all of the releases
-    @Query("SELECT name, coverArtUri FROM releases")
-    suspend fun getAll(): List<ReleaseData>
+    @Query("SELECT * FROM releases")
+    suspend fun getAll(): List<ReleaseEntity>
 }
