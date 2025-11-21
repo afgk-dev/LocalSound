d3d85fd4dba4d68ce082e6557944565c01aa7b35
Felipe <felipe10ferreira2016@gmail.com>
2025-11-21 09:12:38 -0300

    Refactor: Excluded MediaStoreObserver and all content about it in MyApplication

--- app/src/main/java/dev/afgk/localsound/MyApplication.kt
+++ app/src/main/java/dev/afgk/localsound/MyApplication.kt
@@ -1,10 +1,6 @@
 package dev.afgk.localsound
 
 import android.app.Application
-import android.os.Build
-import android.provider.MediaStore
-import android.util.Log
-import dev.afgk.localsound.data.synchronize.MediaStoreObserver
 import dev.afgk.localsound.di.AppModule
 import dev.afgk.localsound.di.AppModuleImpl
 
@@ -13,35 +9,8 @@ class MyApplication : Application() {
         lateinit var appModule: AppModule
     }
 
-    private lateinit var mediaStoreObserver: MediaStoreObserver
-
     override fun onCreate() {
         super.onCreate()
         appModule = AppModuleImpl(this)
-
-        registerMediaStoreObserver()
-    }
-
-    private fun registerMediaStoreObserver() {
-        mediaStoreObserver = MediaStoreObserver(applicationContext)
-
-        val audioUri = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            MediaStore.Audio.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
-        } else {
-            MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        }
-
-        contentResolver.registerContentObserver(
-            audioUri,
-            true,
-            mediaStoreObserver
-        )
-
-        Log.d("MyApplication", "MediaStoreObserver ativo")
-    }
-
-    override fun onTerminate() {
-        contentResolver.unregisterContentObserver(mediaStoreObserver)
-        super.onTerminate()
     }
 }
