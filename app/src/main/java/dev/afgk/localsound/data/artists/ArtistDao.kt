00a231d419fcaeeffe3fb57253b5141c1101cf00
Felipe <felipe10ferreira2016@gmail.com>
2025-11-21 18:09:36 -0300

    Feat: Before delete a track, an artist or release return all the coluns on the logCat. And deleting artists and releases without tracks

--- app/src/main/java/dev/afgk/localsound/data/artists/ArtistDao.kt
+++ app/src/main/java/dev/afgk/localsound/data/artists/ArtistDao.kt
@@ -13,4 +13,16 @@ interface ArtistDao: BaseDao<ArtistEntity> {
     //Get the id of the artist by the name
     @Query("SELECT id FROM artists WHERE name = :name")
     suspend fun getArtistIdByName(name: String?): Long?
+
+    //Get all colluns by Ids
+    @Query("SELECT * FROM artists WHERE id = (:ids)")
+    suspend fun getArtistsById(ids: List<Long>): List<ArtistEntity>
+
+    //Return the ids of artists who have no tracks
+    @Query("SELECT a.id FROM artists AS a JOIN tracks AS t ON (a.id = t.artistId) GROUP BY a.id HAVING COUNT(t.artistId) = 0")
+    suspend fun getIdsOfArtistsWithoutTracks(): List<Long>
+
+    //Delete by ids
+    @Query("DELETE FROM artists WHERE id IN (:ids)")
+    suspend fun deleteArtitsByIds(ids: List<Long>)
 }
