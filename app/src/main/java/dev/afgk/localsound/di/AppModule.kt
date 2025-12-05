a465b25f3131b05da5398b68184f7d89e37cd63b
Felipe <felipe10ferreira2016@gmail.com>
2025-12-05 19:04:56 -0300

    Refactor: Improving the DatabaseSyncWorker by giving the DatabaseSYncronizer as a parameter

--- app/src/main/java/dev/afgk/localsound/di/AppModule.kt
+++ app/src/main/java/dev/afgk/localsound/di/AppModule.kt
@@ -31,6 +31,7 @@ class AppModuleImpl(
         TracksRepository(database.tracksDao())
     }
 
+    //pass this as a parameter to DatabaseSYncWorker
     val databaseSynchronizer: DatabaseSynchronizer by lazy {
         DatabaseSynchronizer(
             mediaStoreRepo = audioFilesRepository,
